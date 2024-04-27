1af7cc8115e5fa9deceaf597a046903eeda508bd
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-04-27 22:32:06 +0200

    Add Migrator

---
+++ src/Migrator/Program.cs
@@ -0,0 +1,6 @@
+﻿// Allows to migrate database.
+
+using Bounan.AniMan.Migrator.Migrations.ReplaceFileIdWithMessageId;
+
+var migration = new ReplaceFileIdWithMessageIdMigration();
+await migration.Run();
